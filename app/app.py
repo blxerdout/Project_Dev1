7f0f42b5e3f204f7fb1bfdd3b025c64fdc47dbe5
blxerdout <blxerdout@gmail.com>
2024-09-17 07:05:07 -0500

    Uploading new files for this project

---
+++ app/app.py
@@ -0,0 +1,12 @@
+
+
+
+class Car:
+    def __init__(self, brand):
+        self.brand = brand 
+
+        car1 = Car("Nissab")
+        car2 = Car("BMW")
+        car3 = Car("Toyota")
+
+        print(car2.brand)
---
+++ app/node.js
@@ -0,0 +1,9 @@
+let my_Name = 'Tafari Hunter'
+
+let fav_Car = [];
+fav_Car.push("Nissan")
+fav_Car.push("BMW")
+fav_Car.push("Toyota")
+
+console.log(fav_Car)
+
